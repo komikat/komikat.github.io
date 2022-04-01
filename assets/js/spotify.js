f663fc702907a75260e057a82d16359d83720d89
Akshit Kumar <41486636+komikat@users.noreply.github.com>
2022-04-01 15:21:42 +0530

    spotify api issues, rolling back

--- assets/js/spotify.js
+++ assets/js/spotify.js
@@ -4,35 +4,39 @@ var artist = null;
 var url = null;
 
 
-const updateSong = async () => {
-    axios.get("https://spotify-now-playing-1.akshitkumar3110.repl.co/now").then(
-        response => {
-            console.log(response.data.playing);
-            if (response.data.playing) {
-                playing = true;
-                song = response.data.song
-                artist = response.data.artist
-                url = response.data.url
+//const updateSong = async () => {
+//    axios.get("https://spotify-now-playing-1.akshitkumar3110.repl.co/now").then(
+//        response => {
+//            console.log(response.data.playing);
+//            if (response.data.playing) {
+//                playing = true;
+//                song = response.data.song
+//                artist = response.data.artist
+//                url = response.data.url
+//
+//
+//
+//                document.getElementById('off').innerText = ""
+//                document.getElementById('song').innerText = song;
+//               document.getElementById('song').href = url;
+//               document.getElementById('artist').innerText = " – " + artist;
+// 
+//             } else {
+//                 document.getElementById('off').innerText = "Offline";
+//                 document.getElementById('song').innerText = "";
+//                 document.getElementById('artist').innerText = "";
+//             }
+//         }
+//     ).catch((err) => {
+//         console.log("error: ", err);
+//         document.getElementById('off').innerText = "Offline";
+//         document.getElementById('song').innerText = "";
+//         document.getElementById('artist').innerText = "";
+//     })
+// }
+//updateSong()
+//setInterval(updateSong, 10000);
 
-
-
-                document.getElementById('off').innerText = ""
-                document.getElementById('song').innerText = song;
-                document.getElementById('song').href = url;
-                document.getElementById('artist').innerText = " – " + artist;
-
-            } else {
-                document.getElementById('off').innerText = "Offline";
-                document.getElementById('song').innerText = "";
-                document.getElementById('artist').innerText = "";
-            }
-        }
-    ).catch((err) => {
-        console.log("error: ", err);
-        document.getElementById('off').innerText = "Offline";
-        document.getElementById('song').innerText = "";
-        document.getElementById('artist').innerText = "";
-    })
-}
-updateSong()
-setInterval(updateSong, 10000);
+document.getElementById('off').innerText = "Offline";
+document.getElementById('song').innerText = "";
+document.getElementById('artist').innerText = "";
